99e25262e320161ebd06b65a7fa64bff753e27ce
God Emperor Joakim <kjronning@gmail.com>
2020-02-13 22:00:12 +0100

    added random fetching for words. onchange gets 10 words at random from the selected size.

--- javascript/hacking.js
+++ javascript/hacking.js
@@ -29,6 +29,28 @@
         return lengthDictionary;
     };
 
+    let selectWords = function(){
+        let length = dropdown.selectedIndex;
+        let dictionary = dictionaries[length];
+        let words = [];
+        let usedIndices = [];
+        console.log(`dictionary length: ${dictionary.length}`);
+        console.log("starting getting random indices");
+        for(let i=0;i<10;i++){
+            let index = Math.floor(Math.random()*dictionary.length);
+            console.log(index);
+            if(!(usedIndices.includes(index))){
+                words.push(dictionary[index]);
+                usedIndices.push(index);
+            }else{
+                console.log(`index ${index} already used.`);
+                i--;
+            }
+        }
+        console.log(`ending getting random indices. List size: ${words.length}`);
+        text.innerHTML = words.join(" ");
+    };
+
     let dropdown = document.getElementById("lengthDropdown");
     let option;
     for (let i=4;i<=13;i++){
@@ -39,8 +61,7 @@
     }
 
     let text = document.getElementById("text");
-    dropdown.onchange = function(){
-        let length = dropdown.selectedIndex;
-        text.innerHTML = dictionaries[length];
-    }
+    dropdown.onchange = selectWords;
+
+
 })();
